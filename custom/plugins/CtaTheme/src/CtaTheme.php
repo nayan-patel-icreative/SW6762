dae40f3f7a19bfeb675becc8e7055c99e6a9f7f9
Nayan Patel <nayan@icreativetechnologies.com>
2026-02-03 10:35:11 +0530

    Add CtaTheme plugin: implement core files, SCSS overrides, and header modifications

---
+++ custom/plugins/CtaTheme/src/CtaTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace CtaTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class CtaTheme extends Plugin implements ThemeInterface
+{
+}
